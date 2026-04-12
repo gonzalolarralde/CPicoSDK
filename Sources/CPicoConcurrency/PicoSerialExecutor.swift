b6faf24c2e286413e949accbdfda1e0e25864398
Gonzalo Larralde <132825+gonzalolarralde@users.noreply.github.com>
2026-04-12 15:49:23 -0400

    Update build config and actor stubs
    Change build settings in Example/build.sh: remove the hardcoded SWIFT_VERSION and set BUILD_TYPE to RelWithDebInfo instead of Debug. In PicoSerialExecutor.swift, comment out the CPU0Actor/CPU1Actor global actor definitions for future exploration and add documentation/TODOs for MainActor describing intended behavior and the need to ensure MainActor tasks run on cpu0 (may require scheduler/runtime changes).

--- Sources/CPicoConcurrency/PicoSerialExecutor.swift
+++ Sources/CPicoConcurrency/PicoSerialExecutor.swift
@@ -1,13 +1,22 @@
-@globalActor
-public actor CPU0Actor {
-    public static let shared = CPU0Actor()
-}
+// This is for a future exploration.
 
-@globalActor
-public actor CPU1Actor {
-    public static let shared = CPU1Actor()
-}
+// @globalActor
+// public actor CPU0Actor {
+//     public static let shared = CPU0Actor()
+// }
 
+// @globalActor
+// public actor CPU1Actor {
+//     public static let shared = CPU1Actor()
+// }
+
+/// The main actor is responsible for executing tasks on the main thread.
+/// This is added just to provide some level of compatibility with existing 
+/// Swift code, but it doesn't have any special meaning in our runtime yet.
+/// 
+/// TODO: Ensure that tasks scheduled on the MainActor actually run on cpu0
+/// and not on a random worker thread. This will likely require some changes
+/// to our runtime scheduler.
 @globalActor
 public actor MainActor {
     public static let shared = MainActor()
