6ca615bbfa56bf95f14f2fb8de32c6fd4694a7f6
Gonzalo Larralde <132825+gonzalolarralde@users.noreply.github.com>
2026-04-26 23:48:00 -0400

    Remove PSRAMAllocator; add PSRAM trait & CShims
    Remove PSRAMAllocator product/targets and its shim, introduce a PSRAM trait and conditional TLSF dependency, and add a new CShims target. Update CPicoSDK target dependencies and reorder/manipulate targets (CPicoConcurrency, ConcurrencyShims, TLSF) in both Package.swift and Package.swift.template to reflect the new PSRAM handling and shims layout.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ import PackageDescription
 let package = Package(
     name: "CPicoSDK",
     products: [
-        .library(name: "CPicoSDK", targets: ["CPicoSDK", "PSRAMAllocator"]),
+        .library(name: "CPicoSDK", targets: ["CPicoSDK"]),
         .library(name: "CPicoConcurrency", targets: ["CPicoConcurrency"]),
         .plugin(name: "PIOASM", targets: ["PIOASMPlugin"]),
         .plugin(name: "PrepareEnvironment", targets: ["PrepareEnvironmentPlugin"]),
@@ -16,6 +16,7 @@ let package = Package(
     ],
     traits: [
         .trait(name: "CPUMetrics", description: "Enables collection of CPU usage metrics in the runtime scheduler. This may have a small performance impact, but can be useful for debugging and optimization. Metrics are available through `CPUStats`."),
+        .trait(name: "PSRAM", description: "Enables support for PSRAM chips, allowing dynamic allocation of memory from the PSRAM."),
 
         // TODO: The generator needs to define traits. This needs to be implemented.
         .trait(name: "Platform_RP2350"),
@@ -57,6 +58,8 @@ let package = Package(
             name: "CPicoSDK",
             dependencies: [
                 .target(name: "ARMClib"),
+                .target(name: "CShims"),
+                .target(name: "TLSF", condition: .when(traits: ["PSRAM"])),
 
                 // GENERATOR MARK: TARGET DEPENDENCIES
                 .target(name: "_CPicoSDK_pico2", condition: .when(traits: ["Variant_RP2350A", "Radio_None"])),
@@ -72,6 +75,17 @@ let package = Package(
 
         // Manually defined targets
         .target(name: "ARMClib"),
+        .target(
+            name: "CShims",
+            dependencies: [
+                .target(name: "_CPicoSDK_pico2", condition: .when(traits: ["Variant_RP2350A", "Radio_None"])),
+                .target(name: "_CPicoSDK_pico2_w", condition: .when(traits: ["Variant_RP2350A", "Radio_CYW43439"])),
+                .target(name: "_CPicoSDK_pimoroni_pico_plus2_rp2350", condition: .when(traits: ["Variant_RP2350B", "Radio_None"])),
+
+                .target(name: "_CPicoSDK_pimoroni_pico_plus2_w_rp2350", condition: .when(traits: ["Variant_RP2350B", "Radio_CYW43439"])),
+            ]
+        ),
+        .target(name: "TLSF"),
 
         .target(
             name: "CPicoConcurrency",
@@ -81,25 +95,6 @@ let package = Package(
             ]
         ),
         .target(name: "ConcurrencyShims"),
-
-        .target(
-            name: "PSRAMAllocator",
-            dependencies: [
-                .target(name: "CPicoSDK"),
-                .target(name: "PSRAMAllocatorShim"),
-                .target(name: "TLSF"),
-            ]
-        ),
-        .target(
-            name: "PSRAMAllocatorShim",
-            dependencies: [
-                .target(name: "_CPicoSDK_pico2", condition: .when(traits: ["Variant_RP2350A", "Radio_None"])),
-                .target(name: "_CPicoSDK_pico2_w", condition: .when(traits: ["Variant_RP2350A", "Radio_CYW43439"])),
-                .target(name: "_CPicoSDK_pimoroni_pico_plus2_rp2350", condition: .when(traits: ["Variant_RP2350B", "Radio_None"])),
-                .target(name: "_CPicoSDK_pimoroni_pico_plus2_w_rp2350", condition: .when(traits: ["Variant_RP2350B", "Radio_CYW43439"])),
-            ]
-        ),
-        .target(name: "TLSF"),
 
         .plugin(
             name: "PIOASMPlugin", 
